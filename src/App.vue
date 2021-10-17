c89269e5da128ab98ae7d090525f5165a72c308b
Joe Fitzsimmons II <jfitzsimmons2@gmail.com>
2021-10-16 20:43:21 -0400

    First

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="hero">
+    <div class="video-container">
+      <video class="video" autoplay loop>
+        <source src="./assets/video/sunset.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div class="hero-content-container">
+      <h1>Joseph Fitzsimmons</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Gotham Narrow SSm A", "Gotham Narrow SSm B";
+  font-size: 18px;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Isotope A", "Isotope B";
+  font-style: normal;
+  font-weight: 900;
+}
+
+.hero {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: auto;
+}
+
+.hero-content-container {
+  padding-left: 1rem;
+}
+
+.video-container {
+  /* width: 900px;
+  flex: 0 1 900px;
+  padding-top: 75%;
+  height: 0px;
+  position: relative; */
+}
+
+.video {
+  width: 100%;
+  height: 100%;
+  /* position: absolute;
+  top: 0;
+  left: 0; */
+}
+</style>
