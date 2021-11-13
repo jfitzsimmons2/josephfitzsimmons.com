e767ad40ad2f4746d8badd9d737ca03247d23930
Joe Fitzsimmons <jfitzsimmons2@gmail.com>
2021-11-13 03:10:25 -0500

    silly updates

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,119 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import SideNav from './components/SideNav.vue'
+import SideNav from "./components/SideNav.vue";
 import menu from "./data/menu";
-
 </script>
 
 <template>
-  <div class="hero">
+  <div class="container">
     <div class="video-container">
       <video class="video" autoplay loop muted>
         <source src="./assets/video/sunset.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
-    <!-- <div class="hero-content-container">
-      <h1>Joseph Fitzsimmons</h1>
-      <p>You can call me Joe. ☕️ </p>
-      <p>I'm a front-end engineer who works with Vue.</p>
-      <p>This is my site where I share what's on my mind.</p>
-    </div> -->
+    <h1 class="text-center">Welcome to the website of Joseph Fitzsimmons</h1>
+    <div style="display: flex; align-items: flex-start; width: 100%">
+      <img src="./assets/images/subzero_from_mortal_kombat.gif" />
+
+      <div style="flex: 1 1 auto; margin: 0 1rem">
+        <p>
+          I'm a front-end software engineer who works with Vue. Welcome to my
+          web page. It's still a little messy here but I'm cleaning things up
+          and I hope you find something useful!
+        </p>
+        <p>
+          I've been making websites since the <strong>90s</strong> when I was in
+          middle school and have had a knack for making things on the web ever
+          since!
+        </p>
+      </div>
+      <img src="./assets/images/REP_MOVE.gif" />
+    </div>
+    <hr />
+    <div style="display: flex; align-items: center">
+      <img src="./assets/images/cd6.gif" style="margin-right: 1rem" />
+      <a href="https://joefitzsimmons.dev/listening/"
+        >Check out what I've been listening to lately</a
+      >
+    </div>
+    <hr />
+    <div style="display: flex">
+      <div style="flex: 1 0 300px">
+        <h2>Things I enjoy</h2>
+        <ul>
+          <li>Cooking delicious foods</li>
+          <li>Making some lo-fi music</li>
+          <li>Playing Rocket League</li>
+        </ul>
+      </div>
+      <div style="flex: 1 1 auto">
+        <div
+          style="
+            background-color: rgba(0, 0, 0, 0.25);
+            border: 2px dashed var(--primary-color);
+            padding: 1rem;
+          "
+        >
+          <p style="margin: 0">
+            This website is a tribute to the web as it was in the 90s and a
+            playground for my Vue experiments. My "real" website that has blog
+            posts on it can be found at
+            <a href="https://joefitzsimmons.dev">joefitzsimmons.dev</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <hr />
+    <div
+      style="
+        display: flex;
+        width: 100%;
+        text-align: center;
+        align-items: center;
+      "
+    >
+      <img
+        src="./assets/images/head_construction.gif"
+        style="width: 50px; margin-right: 1rem"
+      />
+      <span>This website is best viewed on</span>
+      <img src="./assets/images/netscape1.gif" style="margin-left: 1rem" />
+    </div>
   </div>
-  <SideNav :menu="menu.items"></SideNav>
 </template>
 
 <style>
+:root {
+  --global-font-size: 15px;
+  --global-line-height: 1.4em;
+  --global-space: 10px;
+  --font-stack: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+    Bitstream Vera Sans Mono, Courier New, monospace, serif;
+  --mono-font-stack: Menlo, Monaco, Lucida Console, Liberation Mono,
+    DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
+  --background-color: #2f2f2f;
+  --page-width: 60em;
+  --font-color: #f2f2f2;
+  --invert-font-color: #2f2f2f;
+  --primary-color: #e0dd23;
+  --secondary-color: #727578;
+  --error-color: #d20962;
+  --progress-bar-background: #727578;
+  --progress-bar-fill: #151515;
+  --code-bg-color: #1f2224;
+  --input-style: solid;
+  --display-h1-decoration: none;
+}
 *,
 *:before,
 *:after {
   box-sizing: border-box;
+}
+
+img {
+  width: auto;
 }
 
 html {
@@ -41,6 +126,7 @@ body {
   margin: 0;
   display: flex;
   justify-content: center;
+  padding-bottom: 4rem;
 }
 
 h1,
@@ -49,9 +135,13 @@ h3,
 h4,
 h5,
 h6 {
-  font-family: "Isotope A", "Isotope B";
   font-style: normal;
   font-weight: 900;
+  font-size: 1rem;
+}
+/*
+.text-center {
+  text-align: center;
 }
 
 .hero {
@@ -64,7 +154,7 @@ h6 {
 .hero-content-container {
   padding-left: 1rem;
 }
-
+*/
 .video-container {
   /* width: 900px;
   flex: 0 1 900px;
@@ -74,10 +164,17 @@ h6 {
 }
 
 .video {
+  opacity: 0.25;
   width: 100%;
   height: 100%;
+  transition: opacity 0.5s;
   /* position: absolute;
   top: 0;
   left: 0; */
+}
+
+.video:hover {
+  opacity: 0.75;
+  transition: opacity 0.25s;
 }
 </style>
--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,17 +1,18 @@
 <template>
-
-    <ul>
-      <li v-for="item in props.menu" :key="item.key">
-        <SideNavItem :to="item.to" :label="item.label" :items="item.items"></SideNavItem>
-      </li>
-    </ul>
-
+  <ul>
+    <li v-for="item in props.menu" :key="item.key">
+      <SideNavItem
+        :to="item.to"
+        :label="item.label"
+        :items="item.items"
+      ></SideNavItem>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
-import SideNavItem from './SideNavItem.vue';
+import SideNavItem from "./SideNavItem.vue";
 const props = defineProps({
-  menu: Object
+  menu: Object,
 });
 </script>
-
