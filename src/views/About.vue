2daf0fe2eb7ffe55a0684c74a3b805ad40f0d1fa
Joe Fitzsimmons <jfitzsimmons2@gmail.com>
2022-11-27 23:14:09 -0500

    microformats

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="about entry-content">
-    <h1>This is an about page</h1>
-    <a href="https://www.joefitzsimmons.dev">Joe Fitzsimmons.dev</a>
+  <div class="about h-entry">
+    <h1 class="p-name">This is an about page</h1>
+    <p class="p-summary">Summary, testing web mentions</p>
+    <p class="e-content">Trying out this webmention stuff.
+      <a href="https://www.joefitzsimmons.dev">Joe Fitzsimmons.dev</a>
+    </p>
   </div>
 </template>
