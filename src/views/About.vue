d4d463608532ef08ddb2dc885a7b30c5f604a347
Joe Fitzsimmons <jfitzsimmons2@gmail.com>
2022-11-27 23:07:31 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="about entry-content">
     <h1>This is an about page</h1>
     <a href="https://www.joefitzsimmons.dev">Joe Fitzsimmons.dev</a>
   </div>
